8873e0bf61401a999dbb51a2ffb1af047e60afcd
Nii797 <phanterasonpardus797@gmail.com>
2022-01-21 00:15:52 +0700

    modified paragraph in HelloWorld

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,8 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the 
+      Belajar VusJs dan membangun Aplikasi Sinau Koding Academy 
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
     <h3>Installed CLI Plugins</h3>
