02e013f58e2560ed5cfb535e4644b1f3b37cbd34
Nii797 <phanterasonpardus797@gmail.com>
2022-01-21 14:10:57 +0700

    add view

---
+++ src/view/Contact.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="contact">
+        <h1>Contact Kami</h1>
+    </div>
+</template>
---
+++ src/view/Home.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <header>
+            <a href="#"><img src="../assets/img/Sinau-Coding-1.png" alt="Logo"></a>
+            <ul>
+                <li><a href="/" class="active">home</a></li>
+                <li><a href="/about">about</a></li>
+                <li><a href="#">contact</a></li>
+            </ul>
+            <button class="button">join us</button>
+        </header>
+        
+        <main class="main">
+            <div class="group1">
+                <div class="word">
+                    <h2 style="color:  white;">welcome to sinau koding academy</h2>
+                    <p style="color: aliceblue;">Sinau Koding adalah Program Pelatihan Programer dengan materi yang relevan pada kebutuhan industri saat ini. Materi yang ditawarkan diantaranya pemrograman website dan android Lihat</p>
+                </div>
+                <div class="gambar">
+                    <img src="../assets/img/flatdesign 1.png" alt="Gambar">
+                </div>
+            </div>
+        </main>
+        
+        <footer>
+            <div class="copyright">
+                <p>sinau koding academy 2021 &copy;</p>
+            </div>
+            <div class="join-and-tweet">
+                <p>join us</p>
+                <img src="../assets//img/tweet.png" alt="tweet">
+            </div>
+        </footer>
+    </div>
+</template>
