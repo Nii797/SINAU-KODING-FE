bfa00b4eb5695caa6957ca059bfd92b34dd87926
Nii797 <phanterasonpardus797@gmail.com>
2022-01-24 22:58:07 +0700

    add form in component

---
+++ src/components/FormLogin.vue
@@ -0,0 +1,28 @@
+<template>
+
+    <form action="" class="formLogin">
+        <div class="containerlogin">
+                <img src="../assets/img/Sinau-Coding-1.png" alt="Logo">
+                <div class="form-control">
+                    <label for="username">Username</label><br>
+                    <input type="text" name="username" id="username">
+                </div>
+                <div class="form-control">
+                    <label for="username">Username</label><br>
+                    <input type="text" name="username" id="username">
+                </div>
+                <div class="form-control">
+                    <label for="username">Username</label><br>
+                    <input type="text" name="username" id="username">
+                </div>
+                <button class="btn">login</button>
+        </div>
+    </form>
+    
+</template>
+
+<script>
+export default {
+    
+}
+</script>
