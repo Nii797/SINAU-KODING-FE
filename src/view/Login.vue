85a236789480e590afad14fa62059ae68de71cd0
Nii797 <phanterasonpardus797@gmail.com>
2022-01-24 21:07:31 +0700

    update style login form and login landing

---
+++ src/view/Login.vue
@@ -0,0 +1,3 @@
+<template>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis repellat veritatis sint voluptate ullam illo, quaerat vel minima adipisci aut quam neque fugit magnam tenetur quidem. Eveniet cupiditate, nobis, ullam id ad vitae voluptas reprehenderit et facere placeat expedita voluptatum fuga, quo sit tenetur ratione quos illum tempore laudantium officia!</p>
+</template>
