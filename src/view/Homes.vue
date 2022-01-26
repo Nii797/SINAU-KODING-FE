e53a295882e7534caa7d934b876626506731a52b
Nii797 <phanterasonpardus797@gmail.com>
2022-01-26 12:49:18 +0700

    add homes make in bootstrap

---
+++ src/view/Homes.vue
@@ -0,0 +1,68 @@
+<template>
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-transparent">
+          <div class="container">
+            <a class="navbar-brand mt-2" href="#"><img src="../assets/img/Sinau-Coding-1.png" alt="Logo"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse mt-2" id="navbarScroll">
+              <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">About</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Contact</a>
+                </li>
+              </ul>
+              <form class="d-flex">
+                <button class="btn btn-success btn-outline-success text-white" type="submit">Join Us</button>
+              </form>
+            </div>
+          </div>
+        </nav>
+
+        <div class="container">
+          <div class="jumbotron">
+            <div class="row">
+              <div class="col-lg-6 col-md-12">
+                <h2 class="display-4 text-white" style="margin-top: 150px">Welcome to Sinau Koding Academy</h2>
+                <p class="lead text-white">Sinau Koding adalah Program Pelatihan Programer dengan materi yang relevan
+                  pada kebutuhan industri saat ini. Materi yang ditawarkan diantaranya pemrograman website dan android
+                  Lihat</p>
+                <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+              </div>
+              <div class="col-6">
+                <img src="../assets/img/flatdesign 1.png" alt="Gambar">
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <nav class="navbar navbar-expand-lg navbar-dark bg-transparent">
+          <div class="container">
+            <a class="navbar-brand mt-2" href="#">SINAU KODING ACAFDEMY &copy;</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse mt-2" id="navbarScroll">
+              <form class="d-flex align-item-end">
+                <button class="btn btn-success btn-outline-success text-white" type="submit">Join Us</button>
+              </form>
+            </div>
+          </div>
+        </nav>
+
+    </div>
+</template>
+
+<script>
+export default {
+  
+
+
+}
+</script>
