c42cf7f92d45427b40f79f6e5c0deccedb2a044b
Nii797 <phanterasonpardus797@gmail.com>
2022-01-28 10:44:24 +0700

    update all

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from '../view/Home';    
+// import Home from '../view/Home';    
 import Login from '../view/Login';
 import Data from '../view/Data';
 import Homes from '../view/Homes';
@@ -8,10 +8,15 @@ import Homes from '../view/Homes';
 Vue.use(VueRouter);
 
 const routes = [
+    // {
+    //     path: '/',
+    //     name: 'Home',
+    //     component: Home
+    // },
     {
         path: '/',
-        name: 'Home',
-        component: Home
+        name: 'Homes',
+        component: Homes
     },
     {
         path: '/login',
@@ -23,11 +28,11 @@ const routes = [
         name: 'Data',
         component: Data
     },
-    {
-        path: '/homes',
-        name: 'Homes',
-        component: Homes
-    },
+    // {
+    //     path: '/homes',
+    //     name: 'Homes',
+    //     component: Homes
+    // },
 ];
 
 const router = new VueRouter({
